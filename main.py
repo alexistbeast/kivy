c87a35574850a9ba616ed190c8d584902771aabf
alexistbeast <alexisgamer1515@gmail.com>
2025-04-13 13:37:54 +0200

    kivy

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 
+Builder.load_file('whatever.kv')
 
-class MyGridLayout(Widget):
-    name = ObjectProperty(None)
-    pizza = ObjectProperty(None)
-    color = ObjectProperty(None)
+class MyLayout(Widget):
+    pass
 
     # infinite keywords
     def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
-    def press(self):
-        name = self.name.text
-        pizza = self.pizza.text
-        color =  self.color.text
-        #print on the screen
-        #self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favourite pizza and your favourite color is {color} '))
-        print(f'Hello {name}, you like {pizza} pizza, and your favourite color is {color} ')
-
-        #clear the input boxes
-        self.name.text = ""
-        self.pizza.text = ""
-        self.color.text = ""
-class MyApp(App):
+        super(MyLayout, self).__init__(**kwargs)
+class AwesomeApp(App):
     def build(self):
-        return MyGridLayout()
+        Window.clearcolor = (1,1,1,1)
+        return MyLayout()
 
 if __name__ == '__main__':
-    MyApp().run()
+    AwesomeApp().run()
